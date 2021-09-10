fba3b0783c659022e661578bafe7673210f19fa1
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2021-09-10 20:34:10 +0530

    Practice on Sep 10

---
+++ php-basic/req-post-get.php
@@ -0,0 +1,21 @@
+
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+    Name: <input type="text" name="fname">
+    <!-- Number: <input type="text" name="number" placeholder="Phone number"> -->
+    <input type="submit">
+</form>
+
+<?php
+
+    
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // collect value of input field
+        $name = htmlspecialchars($_REQUEST['fname']);
+        if (empty($name)) {
+            echo "Name is empty";
+        } else {
+            echo $name;
+        }
+    }
+?>
+
